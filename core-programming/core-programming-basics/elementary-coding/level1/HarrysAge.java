2215d07c29ebcc33ee1e0e99767f7e073441bd43
shivampratapsi <shivampratapsingh181@gmail.com>
2026-04-05 22:49:36 +0530

    add HarrysAge.java

---
+++ core-programming/core-programming-basics/elementary-coding/level1/HarrysAge.java
@@ -0,0 +1,13 @@
+
+public class HarrysAge {
+    
+   static int getAge(int birthYear , int PresentYear){
+    return PresentYear - birthYear ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( getAge(2000, 2024) );
+        
+    }
+    
+}
