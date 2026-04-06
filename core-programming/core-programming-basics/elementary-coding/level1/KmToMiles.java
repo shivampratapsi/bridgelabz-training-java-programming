7ae57331a90ac8eefd9b308c26d601dfa20eda6e
shivampratapsi <shivampratapsingh181@gmail.com>
2026-04-06 14:36:43 +0530

    add KmToMiles.java

---
+++ core-programming/core-programming-basics/elementary-coding/level1/KmToMiles.java
@@ -0,0 +1,11 @@
+
+public class KmToMiles {
+    static void convertToMiles(float km) {
+        System.out.println("The distance of " + km + "in miles is " + km / 1.6);
+    }
+
+    public static void main(String[] args) {
+        convertToMiles(4);
+    }
+
+}
