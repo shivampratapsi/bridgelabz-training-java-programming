05bb2853cdff09ae3adadfcac3478907bd52286d
shivampratapsi <shivampratapsingh181@gmail.com>
2026-04-06 14:36:11 +0530

    add HeightConversion.java

---
+++ core-programming/core-programming-basics/elementary-coding/level1/HeightConversion.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HeightConversion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float heightInCm = scanner.nextFloat();
+        System.out.println("Your Height in cm is " + heightInCm + " while in feet is " + (heightInCm / 30.68)
+                + "and inches is " + (heightInCm / 2.54));
+                
+
+    }
+}
