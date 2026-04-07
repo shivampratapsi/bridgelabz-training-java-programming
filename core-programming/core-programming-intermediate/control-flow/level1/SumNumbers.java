03f2a9e036bc9509e43db09cf805511296b7dcf2
shivampratapsi <shivampratapsingh181@gmail.com>
2026-04-07 15:17:35 +0530

    add SumNumbers.java

---
+++ core-programming/core-programming-intermediate/control-flow/level1/SumNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long total = 0;
+        while (true) {
+            System.out.println("Enter number : ");
+            long input = scanner.nextLong();
+            if (input <= 0) {
+                break;
+            }
+            total += input;
+        }
+        System.out.printf("Total is %d", total);
+        scanner.close();
+    }
+}
